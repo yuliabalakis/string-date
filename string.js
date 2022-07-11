072fb43a4ed1adc55bafb49625e351397bb3109b
Yulia Balakis <yuliabalakis@gmail.com>
2022-07-11 12:33:05 +0300

    update string.js

--- string.js
+++ string.js
@@ -1,16 +1,16 @@
 /* 1. Given the string 'ahb acb aeb aeeb adcb axeb'. Write a regular expression that matches 
 the strings ahb, acb, aeb by pattern: letter 'a', any character, letter 'b' */
-let str = "ahb acb aeb aeeb adcb axeb";
-let result = /a.b/gi;
-console.log(str.match(result));
+const str = "ahb acb aeb aeeb adcb axeb";
+const regexp = /a.b/gi;
+console.log(str.match(regexp));
 
 /* 2. Given the string '2 + 3 223 2223'. Write a regex that finds line 2 + 3 without capturing the rest */
-let string = "2 + 3 223 2223";
-let res = /2.\+.3/gi;
+const string = "2 + 3 223 2223";
+const res = /2.\+.3/gi;
 console.log(string.match(res));
 
 /* 3. Get the day, month and year of the current date and output it to the console separately */
-let now = new Date();
+const now = new Date();
 console.log(now.getDate());
-console.log(now.getMonth());
+console.log(now.getMonth() + 1);
 console.log(now.getFullYear());
